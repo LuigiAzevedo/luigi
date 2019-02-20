a8376efd97b991070996bb21261d9f68a27d1c6c
Luigi <luigiazevedo97@gmail.com>
2019-02-20 10:22:50 -0300

    Remoção de tratamento

--- valida.php
+++ valida.php
@@ -6,15 +6,6 @@ $senha = hash('md5', $_POST['senha']);
 $select = $mysqli->query("SELECT id, nome, senha FROM teste WHERE nome ='{$_POST['nome']}' AND senha ='{$senha}'");
 $result = $select->fetch_assoc();
 
-if($mysqli->error){  // Caso ocorra algum erro, será impresso na tela
-	try{
-		throw new Exception("MySQL error $mysqli->error <br> Query:<br> $select", $mysqli->error);
-	}	catch(Exception $e) {
-		echo "Error No: ".$e->getCode(). " - ". $e->getMessage() . "<br>";
-		echo nl2br($e->getTraceAsString());
-	}
-}
-
 function redirect($local){   // enviar o usuario para a pagina do parametro
 	header("Location: $local");
 }
@@ -26,9 +17,9 @@ if($select->num_rows === 1){  // checa se o login é valido
 
 	$id = hash('md5', $result['id']);
 	redirect("programa.php");
-}else{
-	echo "<script type='text/javascript'>alert('Login ou senha incorretos. Tente Novamente.')</script>";
+}else{	
 	echo "<META HTTP-EQUIV=\"Refresh\" CONTENT=\"0; URL=luigi.php \">";
+	echo "<script type='text/javascript'>alert('Login ou senha incorretos. Tente Novamente.')</script>";
 }
 $mysqli->close(); // fecha a conecxão
 ?>
