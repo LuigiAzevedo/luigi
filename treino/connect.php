b4b97ba6be76b3eb9c901d3a09fb3f376f5b2d10
Luigi <luigiazevedo97@gmail.com>
2019-06-11 14:22:57 -0300

    Retorno ao estado anterior

--- treino/connect.php
+++ treino/connect.php
@@ -1,8 +1,22 @@
 <?php
-define("DB_SERVER", "adminstock");
-define("DB_USER", "adminstock");
-define("DB_PASSWORD", "Projetopi2019");
-define("DB_DATABASE", "adminstock");
+/*
+define("DB_SERVER", "localhost");
+define("DB_USER", "luigi");
+define("DB_PASSWORD", "calc33XGqqxFfvUj");
+define("DB_DATABASE", "montele_db");
+
+$mysqli = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+if(!$mysqli){
+	echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	exit;
+} */
+
+define("DB_SERVER", "aguiafomento.mysql.uhserver.com");
+define("DB_USER", "aguia12");
+define("DB_PASSWORD", "ThL44frl@A");
+define("DB_DATABASE", "aguiafomento");
 
 $mysqli = mysqli_connect(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
 if(!$mysqli){
@@ -11,4 +25,6 @@ if(!$mysqli){
 	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 	exit;
 }
+
+
 ?>
